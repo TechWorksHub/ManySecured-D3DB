bc9aa93055e1ade38ef8c3869e769b865b44ee63
Craig Rafter <craig@nquiringminds.com>
2022-04-29 15:32:39 +0100

    feat: json filename conversion

--- d3-build/src/json_tools.py
+++ d3-build/src/json_tools.py
@@ -1,10 +1,16 @@
 def check_json_unchanged(file_name: str, data: dict):
-  pass
+    pass
 
 
 def get_json_file_name(yaml_file_name: str):
-  pass
+    json_file_name = (
+        yaml_file_name
+        .replace(".yaml", ".json")
+        .replace("/manufacturers", "/_manufacturers_json")
+        .replace("\\manufacturers", "\\_manufacturers_json")
+    )
+    return json_file_name
 
 
 def write_json(file_name: str, json_data: dict):
-  pass
+    pass

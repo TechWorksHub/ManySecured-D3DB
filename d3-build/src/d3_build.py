a3ac171e75db050190ec6555e723449309c119c8
Craig Rafter <craig@nquiringminds.com>
2022-04-29 15:32:15 +0100

    feat: base compile pipeline

--- d3-build/src/d3_build.py
+++ d3-build/src/d3_build.py
@@ -5,12 +5,13 @@ import multiprocessing as mp
 from d3_utils import process_claim_file
 from guid_tools import get_guid, check_guids
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Get list of YAML files
     file_path = Path(__file__).absolute()
-    yaml_store = Path(file_path / ".."/ ".."/ ".." / "manufacturers").resolve()
+    yaml_store = Path(file_path / ".." / ".." / ".." / "manufacturers")\
+        .resolve()
     files_to_process = [str(fname) for fname in yaml_store.glob("**/*.d3.y*")]
-    
+
     # Create pool for parallel processing
     pool_size = max(mp.cpu_count() - 1, 1)
     pool = mp.Pool(processes=pool_size)
@@ -18,8 +19,8 @@ if __name__ == '__main__':
     # check for duplicate GUID/UUIDs
     guids = pool.map(get_guid, files_to_process)
     check_guids(guids, files_to_process)
-    
+
     # process claims
     pool.map(process_claim_file, files_to_process)
     pool.close()
-    print('Done!')
+    print("Done!")
--- d3-build/src/d3_utils.py
+++ d3-build/src/d3_utils.py
@@ -4,19 +4,19 @@ from json_tools import check_json_unchanged, get_json_file_name, write_json
 from validate_schemas import get_schema_from_path, validate_schema
 from check_uri_resolve import check_uri_resolve
 
+
 def process_claim_file(yaml_file_name: str):
-    print(yaml_file_name)
     # check if file is YAML with right extension
-    is_valid_yaml_claim(yaml_file_name)    
-    
+    is_valid_yaml_claim(yaml_file_name)
+
     json_file_name = get_json_file_name(yaml_file_name)
-    
+
     # import yaml
     yaml_data = read_yaml(yaml_file_name)
-    
+
     # convert to JSON
     json_data = yaml_to_json(yaml_data)
-    
+
     # if JSON already exists and is unchanged then skip
     check_json_unchanged(json_file_name, json_data)
 
@@ -34,4 +34,3 @@ def process_claim_file(yaml_file_name: str):
 
 def yaml_to_json(yaml_data: dict):
     pass
-

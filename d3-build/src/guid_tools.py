cde5eccb5106242834630c32532e7e9384e954f9
antmcc49 <antmcc49@gmail.com>
2022-05-03 11:10:37 +0100

    feat(d3-build): get_guid func

--- d3-build/src/guid_tools.py
+++ d3-build/src/guid_tools.py
@@ -1,4 +1,11 @@
+from yaml_tools import load_claim, is_valid_yaml_claim
+
 def get_guid(file_name: str):
+    if(is_valid_yaml_claim(file_name)):
+        yaml_data = load_claim(file_name)
+        # If the claim exists an ID field
+        if(yaml_data.get("credentialSubject", {}).get("id", False)):
+            return yaml_data['credentialSubject']['id']
     pass
 
 

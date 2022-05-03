ea207204e653e9fdc922e4ef15d22d9207c9efcb
Craig Rafter <craig@nquiringminds.com>
2022-05-03 14:06:55 +0100

    refactor: explict replace dot

--- d3-scripts/src/validate_schemas.py
+++ d3-scripts/src/validate_schemas.py
@@ -6,7 +6,7 @@ from jsonschema import validate
 def get_schema_from_path(yaml_path: str):
     file_path = Path(__file__).absolute()
     schema_store = Path(file_path / ".." / ".." / ".." / "schemas")
-    d3_type = Path(yaml_path).suffixes[0][1:]
+    d3_type = Path(yaml_path).suffixes[0].replace(".", "")
     schema_path = schema_store / f"{d3_type}.json"
     return load_json(str(schema_path.resolve()))
 

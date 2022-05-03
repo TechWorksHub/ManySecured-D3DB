7a30f869f9ec4b323423fa7df1263cf80695a610
Craig Rafter <craig@nquiringminds.com>
2022-05-03 08:57:24 +0100

    chore: refactor utils

--- d3-build/src/yaml_tools.py
+++ d3-build/src/yaml_tools.py
@@ -7,17 +7,21 @@ def is_valid_yaml_claim(file_name: str):
     d3_type, d3_ext, yaml_ext = Path(file_name).suffixes
     assert (
         d3_type[1:] in d3_types
-    ), f"File must have have a valid d3 type extension {d3_types} {file_name}"
-    assert d3_ext == ".d3", f"File must have .d3 extension {file_name}"
-    assert yaml_ext == ".yaml", f"File must have .yaml extension {file_name}"
+    ), f"File has invalid d3 type extension {d3_types} ({file_name})"
+    assert (
+        d3_ext == ".d3"
+    ), f"File missing .d3 extension ({file_name}) e.g. type.d3.yaml"
+    assert (
+        yaml_ext == ".yaml"
+    ), f"File missing .yaml extension ({file_name}) e.g. type.d3.yaml"
     return True
 
 
-def read_yaml(file_name: str):
-yaml_data = {}
-with open(file_name) as f:
-    yaml_data = yaml.safe_load(f)
-    return yaml_data
+def load_claim(file_name: str):
+    yaml_data = {}
+    with open(file_name) as f:
+        yaml_data = yaml.safe_load(f)
+        return yaml_data
 
 
 def lint_yaml(file_name: str):

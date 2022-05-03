19b1d6463f5bb78f45b8f2db159cfc22ba6a59f8
antmcc49 <antmcc49@gmail.com>
2022-05-03 14:43:13 +0100

    docs(d3-build): adding documentation to functions

--- d3-build/src/guid_tools.py
+++ d3-build/src/guid_tools.py
@@ -2,7 +2,16 @@ from yaml_tools import load_claim, is_valid_yaml_claim
 import re
 
 
-def get_guid(file_name: str):
+def get_guid(file_name: str) -> str:
+    """
+    Finds the GUID in a YAML filepath
+
+    Args:
+        file_name: The filepath to the YAML file
+
+    Returns:
+        The GUID found in the YAML file (if it exsist) | None
+    """
     if(is_valid_yaml_claim(file_name)):
         yaml_data = load_claim(file_name)
         # If the claim exists an ID field
@@ -11,11 +20,21 @@ def get_guid(file_name: str):
     pass
 
 
-def check_guids(guids, file_names):
+def check_guids(guids, file_names) -> bool:
+    """
+    Checks all GUIDs are unique and of the correct type
+
+    Args:
+        guids: A list of GUIDs
+        file_name: The filepaths to the YAML files
+
+    Returns:
+        Boolean indicating if the GUIDs are unique and of the correct type
+    """
     # ensure no duplicate GUIDs
     assert (
         len(guids) == len(set(guids))
-    ), f"Duplicate GUIDs found: \n{list_duplicate_guids(guids, file_names)}"
+    ), f"Duplicate GUIDs found: \n{get_duplicate_guids(guids, file_names)}"
 
     # ensure each GUID is a valid UUID
     for guid in guids:
@@ -27,7 +46,17 @@ def check_guids(guids, file_names):
     return True
 
 
-def list_duplicate_guids(guids, file_names):
+def get_duplicate_guids(guids, file_names) -> str:
+    """
+    Function for finding duplicate GUIDs with the file_names found in
+
+    Args:
+        guids: A list of GUIDs
+        file_name: The filepaths to the YAML files
+
+    Returns:
+        String
+    """
     seen = set()
     # adds all elements it doesn't know yet to seen and all other to seen_twice
     seen_before = set(x for x in guids if x in seen or seen.add(x))
@@ -38,7 +67,17 @@ def list_duplicate_guids(guids, file_names):
     ))
 
 
-def find_guid_file_names(guid_id, file_names):
+def find_guid_file_names(guid_id, file_names) -> str:
+    """
+    Function for finding the file name(s) in which a `guid_id` is found
+
+    Args:
+        guids: A list of GUIDs
+        file_name: The filepaths to the YAML files
+
+    Returns:
+        String
+    """
     files = []
     for file_name in file_names:
         if(is_valid_yaml_claim(file_name)):

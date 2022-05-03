18c6b5c50fbe63a2e7c9786bd0a2975e73a8489d
Alois Klink <alois@nquiringminds.com>
2022-05-03 19:09:52 +0100

    feat: add logging levels to d3lint tool
    When running the d3lint CLI, the --quiet and --verbose
    flags can be used to control the verbosity of logging
    messages.

--- d3-scripts/src/d3_scripts/d3_lint.py
+++ d3-scripts/src/d3_scripts/d3_lint.py
@@ -2,10 +2,18 @@
 
 import argparse
 import sys
+import logging
 
 from .d3_utils import validate_d3_claim_files
 
-def cli(argv = None) -> int:
+LOG_LEVELS = {
+    -1: logging.DEBUG,
+    0: logging.INFO,
+    1: logging.WARNING,
+    2: logging.ERROR,
+}
+
+def cli(argv = None):
     parser = argparse.ArgumentParser(
         description = "Lint D3 files for YAML syntax/schema errors",
         epilog = "Example: d3_lint.py *.d3",
@@ -16,7 +24,23 @@ def cli(argv = None) -> int:
         nargs = "+",
         help = "Files to lint",
     )
+    debug_level_group = parser.add_mutually_exclusive_group()
+    debug_level_group.add_argument(
+        "--verbose", "-v",
+        dest="log_level",
+        action="append_const",
+        const=-10,
+    )
+    debug_level_group.add_argument(
+        "--quiet", "-q",
+        dest="log_level",
+        action="append_const",
+        const=10,
+    )
     args = parser.parse_args(argv)
+
+    log_level_sum = min(sum(args.log_level or tuple(), logging.INFO), logging.ERROR)
+    logging.basicConfig(level=log_level_sum)
 
     validate_d3_claim_files(args.D3_FILE)
 

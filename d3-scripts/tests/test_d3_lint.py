a311564d84df06360ce1bd20a7fab1ea55d45eac
Craig Rafter <craig@nquiringminds.com>
2022-05-10 14:52:44 +0100

    fix: linting errors

--- d3-scripts/tests/test_d3_lint.py
+++ d3-scripts/tests/test_d3_lint.py
@@ -2,12 +2,13 @@ import pytest
 
 import d3_scripts.d3_lint
 
+
 def test_lint():
     # should pass with no Exception
     d3_scripts.d3_lint.cli([
         "../manufacturers/AmazonTe/device.type.d3.yaml",
     ])
-    with pytest.raises(Exception) as excinfo:
+    with pytest.raises(Exception):
         d3_scripts.d3_lint.cli([
-            __file__, # this file is a python file, not a valid yaml
+            __file__,  # this file is a python file, not a valid yaml
         ])

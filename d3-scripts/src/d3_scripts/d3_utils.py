d58acf31d696a3c0c8f104062b0b24c0fcf3d605
Ash Setter <ash@nquiringminds.com>
2022-05-31 14:14:30 +0100

    ci: fix ci issues due to line length

--- d3-scripts/src/d3_scripts/d3_utils.py
+++ d3-scripts/src/d3_scripts/d3_utils.py
@@ -164,18 +164,22 @@ def resolve_behaviour_rules(claim, parents):
                         inherited_rule = find_rule(rule, parent_rules)
                         if not inherited_rule:
                             behaviour_id = behaviour["credentialSubject"]["id"]
-                            raise ValueError(f"Non-Existant Rule Error: Behaviour {behaviour_id} attempted to inherit non-existent rule {rule} from behaviour {id}")
+                            raise ValueError(f"""Non-Existant Rule Error: Behaviour {behaviour_id}
+                            attempted to inherit non-existent rule {rule} from behaviour {id}""")
                         existing_rule = find_rule(rule, aggregated_rules)
                         if existing_rule:
                             behaviour_id = behaviour["credentialSubject"]["id"]
-                            raise ValueError(f"Duplicate Rule Error: Behaviour {behaviour_id} attempted to inherit duplicate rule {rule} from behaviour {id}")
+                            raise ValueError(f"""Duplicate Rule Error: Behaviour {behaviour_id}
+                            attempted to inherit duplicate rule {rule} from behaviour {id}""")
                         aggregated_rules += [inherited_rule]
                 else:
                     aggregated_rules += parent_rules
     return aggregated_rules
 
+
 def find_behaviour(id, behaviours):
     return next((item for item in behaviours if item["credentialSubject"]["id"] == id), None)
+
 
 def find_rule(name, rules):
     return next((item for item in rules if item["name"] == name), None)

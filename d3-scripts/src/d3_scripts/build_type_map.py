0b97a870b5cd6de559a39c9894eb707436b14885
Ash Setter <ash@nquiringminds.com>
2022-07-01 13:05:41 +0100

    feat: search for vulnerabilities in TDX dataset and add to types, also adds inheritance of vulnerabilities by default from all parents

--- d3-scripts/src/d3_scripts/build_type_map.py
+++ d3-scripts/src/d3_scripts/build_type_map.py
@@ -1,6 +1,8 @@
 from .claim_graph import build_claim_graph
 import networkx as nx
 
+always_inherited_properties = ["vulnerabilities"]
+
 
 def build_type_map(type_jsons):
     type_map = {claim["credentialSubject"]["id"]: claim for claim in type_jsons}
@@ -8,22 +10,28 @@ def build_type_map(type_jsons):
     sorted_nodes = list(nx.topological_sort(type_graph))
     for type_id in sorted_nodes:
         try:
-            type = type_map[type_id]
+            type_instance = type_map[type_id]
         except KeyError:
             raise KeyError(f"Parent type with id {type_id} of {list(type_graph.predecessors(type_id))} doesn't exist")
-        parents = type["credentialSubject"].get("parents", [])
-        inherited_properties = {}
+        parents = type_instance["credentialSubject"].get("parents", [])
+        type_vulnerabilities = type_instance["credentialSubject"].get("vulnerabilities", [])
+        inherited_properties = {"vulnerabilities": type_vulnerabilities}
         for parent in parents:
             parent_id = parent["id"]
-            for property in parent["properties"]:
-                if property in inherited_properties:
-                    raise KeyError(f"""Duplicate inherited properties in type definition {type_id},
-                    attempted to inherit property `{property}` from multiple parent types""")
+            properties_to_inherit = set(always_inherited_properties + parent["properties"])
+            for property in properties_to_inherit:
                 try:
+                    if property in inherited_properties:
+                        if type(inherited_properties[property]) == list:
+                            inherited_properties[property] + type_map[parent_id]["credentialSubject"][property]
+                        else:
+                            raise KeyError(f"""Duplicate inherited properties in type definition {type_id},
+                            attempted to inherit property `{property}` from multiple parent types""")
                     inherited_properties[property] = type_map[parent_id]["credentialSubject"][property]
                 except KeyError:
                     raise KeyError(f"Attempted to inherit missing property {property} from {parent_id} in {type_id}")
-        type_map[type_id]["credentialSubject"] = {**inherited_properties, **type["credentialSubject"]}
+        inherited_properties["vulnerabilities"] = list(set(inherited_properties["vulnerabilities"]))
+        type_map[type_id]["credentialSubject"] = {**type_instance["credentialSubject"], **inherited_properties}
         type_map[type_id]["credentialSubject"]["children"] = [
             {"id": child_id} for child_id in list(type_graph.successors(type_id))
         ]
--- d3-scripts/src/d3_scripts/d3_build_vulnerabilities.py
+++ d3-scripts/src/d3_scripts/d3_build_vulnerabilities.py
@@ -1,26 +1,64 @@
 import requests
 import json
 import urllib.parse as urllp
+import re
 
 
 def encodeURIComponent(s): return urllp.quote(s, safe="~()*!.'")
 def json_stringify(s): return json.dumps(s, separators=(',', ':'))
 
 
-string_to_search_for = "apple"
+def reproject(vuln):
+    return {
+            "type": "d3-device-type-vuln",
+            "credentialSubject": {
+                "id": vuln["CVE_data_meta"]["ID"],
+                "vulnerability": f"https://nvd.nist.gov/vuln/detail/{vuln['CVE_data_meta']['ID']}"
+            }
+        }
 
-ndJSON = False
-datasetId = "Efl8dagnBm"
-pipeline = [{"$match": {"search_string": {"$regex": string_to_search_for, "$options": "i"}}}]
-format = "aggregate"
-if ndJSON:
-    format = "ndaggregate"
-pipeline = encodeURIComponent(json_stringify(pipeline))
-endpoint = f"resources/{datasetId}/{format}?pipeline={pipeline}"
-queryServer = "https://q.nqm-2.com/v1/"
-query = endpoint
-request = f"{queryServer}{query}"
-print(request)
-response_API = requests.get(request)
-data = json.loads(response_API.text)
-print(data)
+
+def get_vulnerabilities(string_to_search_for, projection=None):
+    ndJSON = False
+    datasetId = "Efl8dagnBm"
+    pipeline = [
+        {"$project": {"search_string": 1, "CVE_data_meta": 1}},
+        {"$match": {"search_string": {"$regex": string_to_search_for, "$options": "i"}}}
+    ]
+    projectionStr = ""
+    if projection is not None:
+        projectionStr = encodeURIComponent(json_stringify(projection))
+    format = "aggregate"
+    if ndJSON:
+        format = "ndaggregate"
+    pipeline = encodeURIComponent(json_stringify(pipeline))
+    endpoint = f"resources/{datasetId}/{format}?pipeline={pipeline}&proj=${projectionStr}"
+    queryServer = "https://q.nqm-2.com/v1/"
+    query = endpoint
+    request = f"{queryServer}{query}"
+    response_API = requests.get(request)
+    data = json.loads(response_API.text)["data"]
+    return data
+
+
+def build_vulnerabilities(type_jsons, pbar=None, percentage_total=None):
+    retrieved_cve_vulnerabilities = []
+    progress_increment = 1/len(type_jsons)
+    for i, type_json in enumerate(type_jsons):
+        pbar.update(progress_increment * percentage_total/100)
+        print(i)
+        search_term = type_json["credentialSubject"].get("modelNumber", None)
+        if search_term is None:
+            search_term = type_json["credentialSubject"].get("name", None)
+        if search_term is not None:
+            cve_vulnerabilities = get_vulnerabilities(re.escape(search_term), {"CVE_data_meta.id": 1})
+            cve_vulnerabilities = [reproject(vuln) for vuln in cve_vulnerabilities]
+            current_vulnerabilities = []
+            try:
+                current_vulnerabilities = type_json["credentialSubject"]["vulnerabilities"]
+            except KeyError:
+                pass
+            cve_vulnerability_ids = [entry["credentialSubject"]["id"] for entry in cve_vulnerabilities]
+            type_json["credentialSubject"]["vulnerabilities"] = current_vulnerabilities + cve_vulnerability_ids
+            retrieved_cve_vulnerabilities += cve_vulnerabilities
+    return retrieved_cve_vulnerabilities
